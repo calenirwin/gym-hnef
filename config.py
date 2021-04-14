957ff4369e6d84ff04cdeeda1d1321c5af46e581
tis13 <47393558+tindur-sigurdarson@users.noreply.github.com>
2021-04-14 18:39:13 -0400

    small fix

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #### SELF PLAY
 EPISODES = 20
 MCTS_SIMS = 20
-MEMORY_SIZE = 3e10
+MEMORY_SIZE = int(3e6)
 TURNS_UNTIL_TAU0 = 10 # turn on which it starts playing deterministically
 CPUCT = 1
 EPSILON = 0.2
