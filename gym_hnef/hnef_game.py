609c85adc0afcab3d25a53ad4378c5b8c073c91d
tis13 <47393558+tindur-sigurdarson@users.noreply.github.com>
2021-04-22 15:48:41 -0400

    without assertion

--- gym_hnef/hnef_game.py
+++ gym_hnef/hnef_game.py
@@ -230,11 +230,11 @@ def next_state(state, action):
     current_player = turn(state)
     
     # assert that the action is valid i.e. that the action is in state[valid_actions]
-    valid_moves = compute_valid_moves(state)
+    # valid_moves = compute_valid_moves(state)
 
-    if action not in valid_moves:
-        print("***Invalid action: ", action)
-        assert False
+    # if action not in valid_moves:
+    #     print("***Invalid action: ", action)
+    #     assert False
 
     if state[current_player][action[0][0]][action[0][1]] == 2:
         state[current_player][action[0][0]][action[0][1]] = 0
