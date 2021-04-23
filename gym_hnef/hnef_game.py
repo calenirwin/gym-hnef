e9378a8dc3d33b28ca3537c750db0aedcd46180f
tis13 <47393558+tindur-sigurdarson@users.noreply.github.com>
2021-04-23 11:40:11 -0400

    no assert in simulate?

--- gym_hnef/hnef_game.py
+++ gym_hnef/hnef_game.py
@@ -431,13 +431,13 @@ def simulate_next_state(state, action):
     current_player = turn(state_copy)
 
     # assert that the action is valid i.e. that the action is in state[valid_actions]
-    valid_moves = compute_valid_moves(state)
+    # valid_moves = compute_valid_moves(state)
 
-    if action not in valid_moves:
-        print("Valid moves: ", valid_moves)
-        print(str(state))
-        print("***Invalid action: ", action)
-        assert False
+    # if action not in valid_moves:
+    #     print("Valid moves: ", valid_moves)
+    #     print(str(state))
+    #     print("***Invalid action: ", action)
+    #     assert False
 
     if state_copy[current_player][action[0][0]][action[0][1]] == 2:
         state_copy[current_player][action[0][0]][action[0][1]] = 0
